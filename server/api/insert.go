a627e77217973623ac874517401173cb0f82c933
MathiasBln <53523648+MathiasBln@users.noreply.github.com>
2024-01-19 16:03:37 +0100

    update test file

--- server/api/insert.go
+++ server/api/insert.go
@@ -1,19 +1,19 @@
-package main
+package api
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
+func InsertIntoHairTest() {
+	//RECUPERER EN MODE POST 
+
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
@@ -34,25 +34,24 @@ func main() {
 		}
 	}()
 
-	coll := client.Database("HairPlanet").Collection("HairTest")
-	name := "Martin"
-	var result bson.M
-	err = coll.FindOne(context.TODO(), bson.D{{"name", name}}).Decode(&result)
-	if err == mongo.ErrNoDocuments {
-		fmt.Printf("No document was found with the name %s\n", name)
-		return
-	}
-	if err != nil {
-		panic(err)
-	}
-
-
-	jsonData, err := json.MarshalIndent(result, "", "    ")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%s\n", jsonData)
-
+	// Call insertOneHairTest
+	insertOneHairTest(client, "Matthieu", 15)
 }
 
+func insertOneHairTest(client *mongo.Client, name string, age int) {
+	coll := client.Database("HairPlanet").Collection("HairTest")
+	// Add Data
+	doc := HairTest{Name: name, Age: age}
 
+	result, err := coll.InsertOne(context.TODO(), doc)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
+}
+
+type HairTest struct {
+	Name string `bson:"name"`
+	Age  int    `bson:"age"`
+}
--- server/api/listByName.go
+++ server/api/listByName.go
@@ -1,4 +1,4 @@
-package main
+package api
 
 import (
 	"context"
@@ -13,50 +13,45 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func Main() {
+func ListByName() ([]byte, error) {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
+		return nil, err
 	}
 
 	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
 		log.Fatal("You must set your 'MONGODB_URI' environment variable.")
+		return nil, fmt.Errorf("MONGODB_URI environment variable not set")
 	}
 
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	defer func() {
 		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
+			log.Printf("Error disconnecting from MongoDB: %v", err)
 		}
 	}()
 
 	coll := client.Database("HairPlanet").Collection("HairTest")
-
-	// Define a filter (empty in this example to get all documents)
-	filter := bson.M{}
-
-	// Find documents in the collection
-	cursor, err := coll.Find(context.TODO(), filter)
+	name := "Martin"
+	var result bson.M
+	err = coll.FindOne(context.TODO(), bson.D{{"name", name}}).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the name %s\n", name)
+		return nil, err
+	}
 	if err != nil {
-		panic(err)
-	}
-	defer cursor.Close(context.TODO())
-
-	// Decode the results
-	var results []bson.M
-	if err := cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	// Print or use the results as JSON
-	jsonData, err := json.MarshalIndent(results, "", "    ")
+	jsonData, err := json.MarshalIndent(result, "", "    ")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	fmt.Printf("%s\n", jsonData)
 
+	return jsonData, nil
 }
